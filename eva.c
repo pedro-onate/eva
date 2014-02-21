5fb124063602f4ce25504970b2060dc5beaa7d44
pedro-onate <pedro.onate@gmail.com>
2014-02-21 14:50:15 -0800

    added sample socket server code

--- eva.c
+++ eva.c
@@ -27,7 +27,8 @@ static struct SymbolTable symtab;
 static struct ScmVal* DEFINE, *LAMBDA, *IF, *BEGIN, *QUOTE;
 struct ScmVal*        SCM_NIL, *SCM_FALSE, *SCM_TRUE, *SCM_UNBOUND, *SCM_UNSPECIFIED, *SCM_EOF;
 
-void* (*alloc)(size_t); 
+void* (*alloc)(size_t);
+static FILE* ostream;
 
 enum ScmType Scm_type(struct ScmVal* exp) {
   if (!exp) {
@@ -226,9 +227,9 @@ static void print_list(struct ScmVal* exp) {
   tail = cdr(exp);
   if (tail != SCM_NIL) {
     if (Scm_type(tail) == PAIR) {
-      printf(" "); print_list(tail);
+      fprintf(ostream, " "); print_list(tail);
     } else {
-      printf(" . "); Scm_print(tail);
+      fprintf(ostream, " . "); Scm_print(tail);
     }
   }
 }
@@ -238,17 +239,17 @@ void Scm_print(struct ScmVal* exp) {
     return;
   }
   switch(Scm_type(exp)) {
-    case NIL:         printf("()");                                      break;
-    case INTEGER:     printf("%ld", SCM_UNTAG(long, exp));               break;
-    case BOOLEAN:     printf("#%c", exp == SCM_TRUE ? 't' : 'f');        break;
-    case PAIR:        printf("("); print_list(exp); printf(")");         break;
-    case SYMBOL:      printf("%s", symtab.symbols[SCM_UNTAG(int, exp)]); break;
-    case CLOSURE:     printf("#<closure>");                              break;
-    case PROCEDURE:   printf("#<procedure>");                            break;
+    case NIL:         fprintf(ostream, "()");                                      break;
+    case INTEGER:     fprintf(ostream, "%ld", SCM_UNTAG(long, exp));               break;
+    case BOOLEAN:     fprintf(ostream, "#%c", exp == SCM_TRUE ? 't' : 'f');        break;
+    case PAIR:        fprintf(ostream, "("); print_list(exp); fprintf(ostream, ")");         break;
+    case SYMBOL:      fprintf(ostream, "%s", symtab.symbols[SCM_UNTAG(int, exp)]); break;
+    case CLOSURE:     fprintf(ostream, "#<closure>");                              break;
+    case PROCEDURE:   fprintf(ostream, "#<procedure>");                            break;
     case UNSPECIFIED:                                                    break;
-    case UNBOUND:     printf("#<unbound>");                              break;
-    case EOFILE:      printf("#<eof>");                                  break;
-    default:          printf("undefined type");                          break;
+    case UNBOUND:     fprintf(ostream, "#<unbound>");                              break;
+    case EOFILE:      fprintf(ostream, "#<eof>");                                  break;
+    default:          fprintf(ostream, "undefined type");                          break;
   }
 }
 
@@ -399,16 +400,160 @@ void Scm_init(size_t heap_size) {
   Scm_define_symbol(env, Scm_Symbol_new("quit"), Scm_Procedure_new(procedure_quit));
 }
 
+/*
 int main() {
-  printf(".------------------.\n");
-  printf("|  EvaScheme v0.1  |\n");
-  printf("'------------------'\n\n");
+  fprintf(ostream, ".------------------.\n");
+  fprintf(ostream, "|  EvaScheme v0.1  |\n");
+  fprintf(ostream, "'------------------'\n\n");
 
   Scm_init(2 << 29);
 
   while (true) {
-    printf("eva> ");
+    fprintf(ostream, "eva> ");
     Scm_print(Scm_eval(Scm_parse(stdin), env));
-    printf("\n"); 
+    fprintf(ostream, "\n"); 
   }
+}
+*/
+
+/*
+** server.c -- a stream socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define PORT "3490"  // the port users will be connecting to
+
+#define BACKLOG 10     // how many pending connections queue will hold
+
+void sigchld_handler(int s)
+{
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(void)
+{
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+
+    printf("server: waiting for connections...\n");
+
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
+
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        printf("server: got connection from %s\n", s);
+
+        if (!fork()) { // this is the child process
+          close(sockfd); // child doesn't need the listener
+          /*if (send(new_fd, "Hello, world!", 13, 0) == -1)
+              perror("send");
+          close(new_fd);
+          exit(0);*/
+          FILE* iostream = fdopen(new_fd, "r+");
+          ostream = iostream;
+
+          fprintf(ostream, ".------------------.\n");
+          fprintf(ostream, "|  EvaScheme v0.1  |\n");
+          fprintf(ostream, "'------------------'\n\n");
+
+          Scm_init(2 << 29);
+
+          while (true) {
+            fprintf(ostream, "eva> ");
+            Scm_print(Scm_eval(Scm_parse(iostream), env));
+            fprintf(ostream, "\n"); 
+          }
+        }
+        close(new_fd);  // parent doesn't need this
+    }
+
+    return 0;
 }
